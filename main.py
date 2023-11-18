19d0e5125b64c0664e4c3db4dd0e23345c1bb798
Newton23-nk <newtonkimathi20@gmail.com>
2023-11-18 16:33:25 +0300

    Youtube Assistant

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import langchain_helper as lch
 import textwrap
 
 st.title("YouTube Assistant")
+st.write("""I'm here to assist you in answering questions about the Youtube video you share. 
+            Just paste the link to the Youtube video and feel free to ask me anything!""")
 
+st.image("Images/Youtube_Helper.jpg")
 with st.sidebar:
     with st.form(key='my_form'):
         youtube_url = st.sidebar.text_area(
@@ -18,11 +21,11 @@ with st.sidebar:
         openai_api_key = st.sidebar.text_input(
             label="OpenAI API Key",
             key="langchain_search_api_key_openai",
-            max_chars=50,
+            max_chars=100,
             type="password"
             )
         "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-        "[View the source code](https://github.com/rishabkumar7/pets-name-langchain/tree/main)"
+        "[View the source code](https://github.com/Newton23-nk/Youtube_Helper_Langchain)"
         submit_button = st.form_submit_button(label='Submit')
 
 if query and youtube_url:
